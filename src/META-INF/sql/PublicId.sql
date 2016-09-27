561870182e5df960df88cb09e57d664e7f62b42f
Zakrom <edgararturo@msn.com>
2016-09-27 11:56:05 -0500

    Add store to database methods for PublicId

--- src/META-INF/sql/PublicId.sql
+++ src/META-INF/sql/PublicId.sql
@@ -4,6 +4,12 @@ INSERT INTO rdap.public_id VALUES (null,?,?);
 #storeDomainPublicIdsToDatabase
 INSERT INTO  rdap.domain_public_ids VALUES (?,?)
 
+#storeEntityPublicIdsToDatabase
+INSERT INTO  rdap.entity_public_ids VALUES (?,?)
+
+#storeRegistrarPublicIdsToDatabase
+INSERT INTO  rdap.registrar_public_ids VALUES (?,?)
+
 #getAll
 SELECT * FROM rdap.public_id
 
